67cf4b603a5aac62604753cccda53f0a0a508bea
4SH1N <ash91p@proton.me>
2026-06-25 20:21:37 +0100

    Fill the appropriate actions for tasks and todos controllers and add before action inside tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,33 +1,51 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[edit update destroy]
+  before_action :set_task_todos, only: %i[show]
+
   def show
-    @task = Task.includes(:todos).find(params[:id])
   end
 
   def new
+    @task = current_user.tasks.new
   end
 
   def edit
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to @task, notice: "You have updated your task successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def create
-    @task = current_user.task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
-      redirect_to task_path(@task), notice: "You have created you task successfully"
+      redirect_to @task, notice: "You have created you task successfully"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
+    @task.destroy
+    redirect_to task_path
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :description, :due_date, :priority)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def set_task_todos
+    @task = current_user.tasks.includes(:todos).find(params[:id])
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,16 +1,34 @@
 class TodosController < ApplicationController
   def edit
+    @task = current_user.tasks.find(params[:task_id])
+    @todo = @task.todos.find(params[:id])
   end
 
   def new
+    @task = current_user.tasks.find(params[:task_id])
+    @todo = @task.todos.new
   end
 
   def update
+    @task = current_user.tasks.find(params[:task_id])
+    @todo = @task.todos.find(params[:id])
+
+    if @todo.update(todo_params)
+      redirect_to @task, notice: "You have updated the todo list successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def create
-    @task = current_user.task.find(params[:task_id])
+    @task = current_user.tasks.find(params[:task_id])
     @todo = @task.todos.new(todo_params)
+
+    if @todo.save
+      redirect_to @task, notice: "You have cerated your todo successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -19,6 +37,6 @@ class TodosController < ApplicationController
   private
 
   def todo_params
-    params.(:todo).permit(:content, :status)
+    params.require(:todo).permit(:content, :status)
   end
 end

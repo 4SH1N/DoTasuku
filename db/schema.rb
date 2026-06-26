fc08e8eee7c931f4029f780abf7f3ee4dbea7d29
4SH1N <ash91p@proton.me>
2026-06-26 01:23:10 +0100

    Add migrations of user to tasks and tasks to todos and edit the tasks view to have a form and show the created elements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_25_121436) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_25_234928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,13 +21,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_25_121436) do
     t.integer "priority"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "todos", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.boolean "status"
+    t.bigint "task_id", null: false
     t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_todos_on_task_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +46,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_25_121436) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "tasks", "users"
+  add_foreign_key "todos", "tasks"
 end
